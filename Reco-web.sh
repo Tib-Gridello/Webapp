4f583ba7f4bc9932823ffda03cca92586731007b
Tib <34336452+Tib-Gridello@users.noreply.github.com>
2022-02-06 16:26:43 +0000

    Update Reco-web.sh

--- Reco-web.sh
+++ Reco-web.sh
@@ -4,7 +4,8 @@ Reconnaissance () {
 
         #ffuf using raft-large-directories
         arg1=$1
-        ffuf -u $arg1/FUZZ -w "/usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt" -recursion -e ".bak, .zip, .bak, .sql, .zip, .xml, .old, .inc, .js, .json, .conf, .cfg, .log, .yml, .yaml, .txt, .sql, .rar, .bak-ac, .aspx, .php" -recursion-depth 2 -replay-proxy http://127.0.0.1:8080 -c -o $Full_path/ffuf-dir.txt  
+        arg2=$(basename $arg1)
+        ffuf -u $arg1/FUZZ -w "/usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt" -recursion -e ".bak, .zip, .bak, .sql, .zip, .xml, .old, .inc, .js, .json, .conf, .cfg, .log, .yml, .yaml, .txt, .sql, .rar, .bak-ac, .aspx, .php" -recursion-depth 2 -replay-proxy http://127.0.0.1:8080 -c -o $Full_path/ffuf-dir.txt -fs 0 -fc 404 
 
         #ffuf using raft-large-files
         ffuf -u $arg1/FUZZ -w "/usr/share/seclists/Discovery/Web-Content/raft-large-files.txt" -recursion -e ".bak, .zip, .bak, .sql, .zip, .xml, .old, .inc, .js, .json, .conf, .cfg, .log, .yml, .yaml, .txt, .sql, .rar, .bak-ac, .aspx, .php" -recursion-depth 2 -replay-proxy http://127.0.0.1:8080 -c -o $Full_path/ffuf-files.txt 
@@ -14,18 +15,12 @@ Reconnaissance () {
 
         #nuclei on the target
         nuclei -update 
-        nuclei $arg1 -o $Full_path/nuclei-output
+        nuclei -u $arg1 -o $Full_path/nuclei-output
+        ip_target=$(host $arg2 | awk '/has address/ { print $4 ; exit }')
+        nmap -sV -A -p- $ip_target -oA $Full_path/nmap-output 
 }
-
 Base_path=~/Desktop
 Folder_name=$(basename $1)
 Full_path=${Base_path}/${Folder_name}
-
-if [ -d "$DIR" ]; then
-        echo "fdppppp"$1
-        Reconnaissance $1
-else 
-        mkdir -p $Full_path
-        echo "fdppppp2" $1
-        Reconnaissance $1
-fi
+mkdir -p $Full_path
+Reconnaissance $1
